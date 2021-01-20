70f98e9012ce80b8a7e503414f11b8ef820e7eb4
Jacob Chen <chenjr0719@gmail.com>
2021-01-20 11:15:18 +0800

    test: Add test for training

--- tests/test_captchacracker.py
+++ tests/test_captchacracker.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from captchacracker.model import CaptchaCracker
 
 
@@ -7,3 +9,14 @@ def test_inference(random_str, img):
 
     assert isinstance(output, str)
     assert output == random_str
+
+
+def test_train():
+    save_path = "lighter_backbone_pretrained/crnn_ctc_model.pth"
+    model = CaptchaCracker()
+
+    model.fit(
+        batch_size=16, max_iterations=10, iteration_per_epoch=20, save_path=save_path,
+    )
+
+    assert Path(save_path).is_file()
